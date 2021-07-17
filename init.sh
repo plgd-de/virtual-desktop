1efc4ab03be39315727439ad96068b298af40641
Michael Pajewski <80986335+pajew-ski@users.noreply.github.com>
2021-07-17 19:52:13 +0200

    Update init.sh

--- init.sh
+++ init.sh
@@ -13,20 +13,20 @@ chmod +x /home/magenta/.Xclients
 
 systemctl restart xrdp 
 
-#grep -v  "show_trash=1" /home/magenta/.config/pcmanfm/LXDE/desktop-items-0.conf > temp
-#mv temp /home/magenta/.config/pcmanfm/LXDE/desktop-items-0.conf
-#echo "show_trash=0" >> /home/magenta/.config/pcmanfm/LXDE/desktop-items-0.conf
+grep -v  "show_trash=1" /home/magenta/.config/pcmanfm/LXDE/desktop-items-0.conf > temp
+mv temp /home/magenta/.config/pcmanfm/LXDE/desktop-items-0.conf
+echo "show_trash=0" >> /home/magenta/.config/pcmanfm/LXDE/desktop-items-0.conf
 
-#wget https://raw.githubusercontent.com/playgroun-de/virtual-desktop/main/panel.conf
-#cp panel.conf /home/magenta/.config/lxpanel/LXDE/panels/panel
-#chown magenta:magenta /home/magenta/.config/lxpanel/LXDE/panels/panel
+wget https://raw.githubusercontent.com/playgroun-de/virtual-desktop/main/panel.conf
+cp panel.conf /home/magenta/.config/lxpanel/LXDE/panels/panel
+chown magenta:magenta /home/magenta/.config/lxpanel/LXDE/panels/panel
 
-#apt remove clipit -y
-#apt install htop neofetch nmap tmux tldr -y
+apt remove clipit -y
+apt install htop neofetch nmap tmux tldr -y
 
 echo "init abgeschlossen"
 
-#rm ./panel.conf
+rm ./panel.conf
 rm ./init.sh
 
 echo "temporäre downloads entfernt"
