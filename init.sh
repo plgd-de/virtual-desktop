a959105a73ee068ea0764cda09d0dc75989e233a
Michael Pajewski <80986335+pajew-ski@users.noreply.github.com>
2021-07-17 19:30:16 +0200

    Update init.sh

--- init.sh
+++ init.sh
@@ -19,13 +19,15 @@ chmod +x /home/magenta/.Xclients
 
 systemctl restart xrdp
 
-grep -v  "show_trash=1" /home/magenta/.config/pcmanfm/LXDE/desktop-items-0.conf > temp
-mv temp /home/magenta/.config/pcmanfm/LXDE/desktop-items-0.conf
-echo "show_trash=0" >> /home/magenta/.config/pcmanfm/LXDE/desktop-items-0.conf
+nano 
 
-wget https://raw.githubusercontent.com/playgroun-de/virtual-desktop/main/panel.conf
-cp panel.conf /home/magenta/.config/lxpanel/LXDE/panels/panel
-chown magenta:magenta /home/magenta/.config/lxpanel/LXDE/panels/panel
+#grep -v  "show_trash=1" /home/magenta/.config/pcmanfm/LXDE/desktop-items-0.conf > temp
+#mv temp /home/magenta/.config/pcmanfm/LXDE/desktop-items-0.conf
+#echo "show_trash=0" >> /home/magenta/.config/pcmanfm/LXDE/desktop-items-0.conf
+
+#wget https://raw.githubusercontent.com/playgroun-de/virtual-desktop/main/panel.conf
+#cp panel.conf /home/magenta/.config/lxpanel/LXDE/panels/panel
+#chown magenta:magenta /home/magenta/.config/lxpanel/LXDE/panels/panel
 
 echo "init abgeschlossen"
 
