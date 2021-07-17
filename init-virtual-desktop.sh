956449d837e98266f60a3035dbd172a332e171b6
Michael Pajewski <80986335+pajew-ski@users.noreply.github.com>
2021-07-17 18:01:58 +0200

    Update init-virtual-desktop.sh

--- init-virtual-desktop.sh
+++ init-virtual-desktop.sh
@@ -15,6 +15,10 @@ grep -v  "show_trash=1" /home/magenta/.config/pcmanfm/LXDE/desktop-items-0.conf 
 mv temp /home/magenta/.config/pcmanfm/LXDE/desktop-items-0.conf
 echo "show_trash=0" >> /home/magenta/.config/pcmanfm/LXDE/desktop-items-0.conf
 
+wget https://raw.githubusercontent.com/playgroun-de/virtual-desktop/main/panel.conf
+cp panel.conf /home/magenta/.config/lxpanel/LXDE/panels/panel
+chown magenta:magenta /home/magenta/.config/lxpanel/LXDE/panels/panel
+
 echo "lxsession -s LXDE -e LXDE" > /home/magenta/.Xclients
 chown magenta:magenta /home/magenta/.Xclients
 chmod +x /home/magenta/.Xclients
