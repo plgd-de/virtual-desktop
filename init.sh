e4ff8d795deabbd9efe80eb7f0f84435c622825c
Michael Pajewski <80986335+pajew-ski@users.noreply.github.com>
2021-07-17 19:18:54 +0200

    Update init.sh

--- init.sh
+++ init.sh
@@ -13,15 +13,15 @@ apt install neofetch -y
 useradd magenta; echo -e "Magenta1\nMagenta1" | passwd magenta
 adduser magenta netdev
 
-grep -v  "show_trash=1" /home/magenta/.config/pcmanfm/LXDE/desktop-items-0.conf > temp
-mv temp /home/magenta/.config/pcmanfm/LXDE/desktop-items-0.conf
-echo "show_trash=0" >> /home/magenta/.config/pcmanfm/LXDE/desktop-items-0.conf
-
 echo "lxsession -s LXDE -e LXDE" > /home/magenta/.Xclients
 chown magenta:magenta /home/magenta/.Xclients
 chmod +x /home/magenta/.Xclients
 
 systemctl restart xrdp
+
+grep -v  "show_trash=1" /home/magenta/.config/pcmanfm/LXDE/desktop-items-0.conf > temp
+mv temp /home/magenta/.config/pcmanfm/LXDE/desktop-items-0.conf
+echo "show_trash=0" >> /home/magenta/.config/pcmanfm/LXDE/desktop-items-0.conf
 
 wget https://raw.githubusercontent.com/playgroun-de/virtual-desktop/main/panel.conf
 cp panel.conf /home/magenta/.config/lxpanel/LXDE/panels/panel
