1e1832d6db28d441fd8244cb9d4c69677e73b094
Michael Pajewski <80986335+pajew-ski@users.noreply.github.com>
2021-07-17 17:14:18 +0200

    Update init-virtual-desktop.sh
    Remove Trashcan

--- init-virtual-desktop.sh
+++ init-virtual-desktop.sh
@@ -9,6 +9,12 @@ useradd -m magenta
 passwd magenta
 adduser magenta netdev
 
+nano /home/magenta/.config/pcmanfm/LXDE/desktop-items-0.conf
+
+grep -v  "show_trash=1" /home/magenta/.config/pcmanfm/LXDE/desktop-items-0.conf > temp
+mv temp /home/magenta/.config/pcmanfm/LXDE/desktop-items-0.conf
+echo "show_trash=0" >> /home/magenta/.config/pcmanfm/LXDE/desktop-items-0.conf
+
 echo "lxsession -s LXDE -e LXDE" > /home/magenta/.Xclients
 chown magenta:magenta /home/magenta/.Xclients
 chmod +x /home/magenta/.Xclients
